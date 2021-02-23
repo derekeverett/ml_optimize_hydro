db4d8e8410d837a025d289642ef79f82feb9c503
Derek Everett <osu9790@owens-rw01.ten.osc.edu>
2021-02-23 15:19:50 -0500

    first trials with energy density profiles, now need to try increasing resolution, adding more Conv2D layers, consider adding a Guassian smoothing kernel, ...

---
+++ predict_profiles/IPGANsma/prepare.sh
@@ -0,0 +1,10 @@
+
+#download the training events to disk
+mkdir training_profiles
+cd training_profiles
+wget http://quark.phy.bnl.gov/~bschenke/ipglasma-events.tar.gz
+tar -xvf ipglasma-events.tar.gz
+cd ..
+
+#make a directory to store trained GANs
+mkdir generators
